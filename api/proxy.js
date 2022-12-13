cb78f59e11be4fdb4af9263e942107c719ea0c7d
Adam55001 <80415514+Adam55001@users.noreply.github.com>
2022-12-13 10:00:19 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.google.com/";
+  let target = "https://uv-app3.adam55002.repl.co";
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   //   if (
